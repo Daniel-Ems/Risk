4ee4912379c919e146d3b7f3f904572099749b28
Daniel-Ems <danielems3@outlook.com>
2017-10-26 11:03:14 -0400

    periodic commit

---
+++ risk_client.py
@@ -0,0 +1,19 @@
+#/usr/bin/env pyhton3 
+import inspect, os, sys
+from socket import *
+
+def main():
+    host, port = 'localhost', 5555
+
+    with socket(AF_INET, SOCK_STREAM) as sfd:
+        sfd.connect((host,port))
+        print("Connected To:", sfd.getpeername(),
+                  "From:", sfd.getsockname())
+
+        while True:
+            data = sfd.recv(1024)
+            if data.decode() == "quit": break
+            text = input(data.decode())
+            if text.lower() == "quit": break
+            sfd.send(text.encode()) 
+if __name__ == "__main__": main()
