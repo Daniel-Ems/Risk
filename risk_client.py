b4e1a077fd1ba166e5634244784e2aa55a17791e
Daniel-Ems <danielems3@outlook.com>
2017-10-27 15:49:06 -0400

    update some py thangs

--- risk_client.py
+++ risk_client.py
@@ -9,9 +9,13 @@ def main():
         sfd.connect((host,port))
         print("Connected To:", sfd.getpeername(),
                   "From:", sfd.getsockname())
+        err = 'ERR: '
 
         while True:
             data = sfd.recv(1024)
+            if "output" in data.decode():
+                print(data.decode())
+                continue;
             if data.decode() == "quit": break
             text = input(data.decode())
             if text.lower() == "quit": break
